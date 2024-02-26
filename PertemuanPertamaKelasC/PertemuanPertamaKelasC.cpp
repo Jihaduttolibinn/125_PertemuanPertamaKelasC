1ae4f03b019182d2f9d72da9e773fd2def150448
jihad <jihaduttolibin02@gmail.com>
2024-02-26 15:41:57 +0700

    Add project files.

---
+++ PertemuanPertamaKelasC/PertemuanPertamaKelasC.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//begin
+	// numeric nJejari, nLuas
+	// display ' Masukan jejari= '
+	// accept njejari
+	// compute nLuas = 3.14 * r * r
+	// display 'Luasnya= '+ nLuas
+//end
+	double nJejari, nLuas;
+	cout << "Masukan Jejari = ";
+	cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+	cout << "Luasnya = " << nLuas << endl;
+	system("pause");
+
+
+
+}
+
+
+
